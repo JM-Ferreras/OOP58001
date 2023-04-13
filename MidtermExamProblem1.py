f5c8bbb5f57c7f7bb4f5677e92a21bb72d88f15c
Migz-Ferreras <123426641+JM-Ferreras@users.noreply.github.com>
2023-04-13 08:52:03 +0800

    Add files via upload

---
+++ MidtermExamProblem1.py
@@ -0,0 +1,16 @@
+import math
+
+class Circle:
+  def __init__(attributes, radius):
+    attributes.radius=radius
+    attributes.pi=math.pi
+  def perimeter(attributes):
+    return 2*attributes.pi*attributes.radius
+  def area(attributes):
+    return attributes.pi*(attributes.radius**2)
+  def display(attributes):
+    print("The given circle has a perimeter of", attributes.perimeter())
+    print("And a circumference of", attributes.area())
+
+samplecircle=Circle(float(input("Give a radius: ")))
+samplecircle.display()
